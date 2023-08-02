20b2381ad9c35f32cc17a760b820f92ce3124259
VAL PIMENTEL <valpimentel1019@gmail.com>
2023-08-02 13:07:23 -0600

    Create untitled.m

---
+++ Examen 2do parcial/untitled.m
@@ -0,0 +1,67 @@
+clc;
+% for o=1:30
+%     o
+clear
+% Parámetros del algoritmo DE
+var = 5;
+NP = 11;  % Tamaño de la población
+F = 0.68;  % Factor de amplificación-muta
+CR = 0.68; % Tasa de recombinación
+% O = 1e-5;
+generaciones = 610; % Número de generaciones
+
+% Límites de las variables
+
+li = [1, 1, 1, 1, 1];
+
+ls = [20 30 40 60 80];
+
+% Generar la población inicial
+poblacion = gen_p(li,ls,var,NP);
+
+poblacion = FO(poblacion,NP,var);
+
+
+for g=1:generaciones
+    g
+    for i=1:NP
+        % Seleccionar tres padres aleatorios distintos entre si, te amo <3
+        n = 1:NP;
+        n = setdiff(n,i);
+        padres = n(randperm(NP-1,3));
+        
+        %Generar Ruido
+        v = poblacion(padres(1),1:var) + F*(poblacion(padres(2),1:var) - poblacion(padres(3),1:var));
+        
+        % Corregir desbordamiento
+        v = corregir_desbordamiento(v, li, ls);
+        
+        %Realizar la recombinación binomial
+        u = poblacion(i,1:var);
+        j_rand = randi(var);
+        for j = 1:var
+            if j == j_rand || rand() < CR
+                u(j) = v(j);
+            end
+        end
+        
+        %Evaluar FO y SVR
+        u = FO(u,1,var);
+        
+        %Seleccion con DEV
+        poblacion(i,:) = DEB(poblacion(i,:),u,var);
+    end
+    poblacion    
+end
+
+% end 
+[best, idx] = min(poblacion(:,var+1));
+x_best = poblacion(idx,:);
+
+% Imprimir la mejor solución y su valor de función objetivo
+disp("La mejor solución es:")
+disp(x_best)
+disp("Valor de función objetivo:")
+disp(best)
+
+
